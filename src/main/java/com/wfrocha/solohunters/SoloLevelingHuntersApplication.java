ddd4023eb44442cb7888d55cd58996b325cd3331
Waldey <waldeypereira@gmail.com>
2025-08-07 17:55:28 +0200

    first commit

---
+++ src/main/java/com/wfrocha/solohunters/SoloLevelingHuntersApplication.java
@@ -0,0 +1,13 @@
+package com.wfrocha.solohunters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloLevelingHuntersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloLevelingHuntersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wfrocha/solohunters/SoloLevelingHuntersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfrocha.solohunters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloLevelingHuntersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
