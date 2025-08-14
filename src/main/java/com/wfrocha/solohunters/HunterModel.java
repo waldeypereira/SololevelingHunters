c2510cb676d51f10c207f24dd81ec9fc419b4d8e
Waldey <waldeypereira@gmail.com>
2025-08-14 11:53:39 +0200

    Cria uma entidade no banco de dados a partir do HunterModel

---
+++ src/main/java/com/wfrocha/solohunters/HunterModel.java
@@ -0,0 +1,71 @@
+package com.wfrocha.solohunters;
+
+
+import jakarta.persistence.*;
+
+// Ele transforma uma classe Java em uma entidade JPA, que pode ser mapeada para uma tabela no banco de dados.
+@Entity
+@Table(name = "tb_hunters")
+public class HunterModel {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "id_hunter", nullable = false)
+    // Ele define o campo "id_hunter" como a chave primária da tabela "
+    private Long id;
+    private String name;
+    private int age;
+    private String nationality;
+    private String rank;
+    private String guild;
+
+    public HunterModel() {
+    }
+
+    public HunterModel(String name, int age, String nationality, String rank, String guild) {
+        this.name = name;
+        this.age = age;
+        this.nationality = nationality;
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getGuild() {
+        return guild;
+    }
+
+    public void setGuild(String guild) {
+        this.guild = guild;
+    }
+}
